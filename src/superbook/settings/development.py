be91a1577886322426e81544ca2a3b58088aacde
Arun Ravindran <arunvr@gmail.com>
2018-05-15 23:00:27 +0530

    API shows username instead of primary id

--- src/superbook/settings/development.py
+++ src/superbook/settings/development.py
@@ -39,6 +39,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 INTERNAL_IPS = [
     '127.0.0.1',
     '0.0.0.1',
+    'localhost',
     'testserver',
 ]
 
--- src/viewschapter/serializers.py
+++ src/viewschapter/serializers.py
@@ -3,6 +3,11 @@ from posts import models
 
 
 class PostSerializer(serializers.ModelSerializer):
+    posted_by = serializers.SerializerMethodField()
+
+    def get_posted_by(self, obj):
+        return obj.posted_by.username
+
     class Meta:
         model = models.Post
-        fields = ("posted_by_id", "message")
+        fields = ("posted_by", "message",)
