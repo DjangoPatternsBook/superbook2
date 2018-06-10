8e992f34d2ed0c80d98ed25d3f897ba2496ea917
Arun Ravindran <arunvr@gmail.com>
2018-06-10 11:45:28 +0530

    Fixes Issue #1 to redirect to home page

--- src/superbook/settings/base.py
+++ src/superbook/settings/base.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 
 
 # What user sees after logging in
-LOGIN_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = "viewschapter:myfeed"
 
 # Crispy Form Theme - Bootstrap 4
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
