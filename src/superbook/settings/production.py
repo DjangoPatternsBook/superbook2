b773476f683f2c84988d9d990ddaf1884d83a12c
Arun Ravindran <arunvr@gmail.com>
2018-06-11 17:15:27 +0530

    Added production logging settings

--- src/superbook/settings/production.py
+++ src/superbook/settings/production.py
@@ -23,22 +23,50 @@ DEBUG = os.environ.get('DEBUG', False)
 
 WSGI_APPLICATION = 'src.superbook.wsgi.application'
 
-# in your settings file
-LOGGING = {
+# Logging
+import logging.config
+from django.utils.log import DEFAULT_LOGGING
+
+# Disable Django's logging setup
+LOGGING_CONFIG = None
+
+LOGLEVEL = os.environ.get('LOGLEVEL', 'info').upper()
+
+logging.config.dictConfig({
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            # exact format is not important, this is the minimum information
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        },
+        'django.server': DEFAULT_LOGGING['formatters']['django.server'],
+    },
     'handlers': {
+        # console logs to stderr
         'console': {
             'class': 'logging.StreamHandler',
+            'formatter': 'default',
         },
+        'django.server': DEFAULT_LOGGING['handlers']['django.server'],
     },
     'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        # default for all undefined Python modules
+        '': {
+            'level': 'WARNING',
+            'handlers': ['console', ],
         },
+        # Our application code
+        'app': {
+            'level': LOGLEVEL,
+            'handlers': ['console', ],
+            # Avoid double logging because of root logger
+            'propagate': False,
+        },
+        # Default runserver request logging
+        'django.server': DEFAULT_LOGGING['loggers']['django.server'],
     },
-}
+})
 
 # Configure Django App for Heroku.
 import django_heroku
