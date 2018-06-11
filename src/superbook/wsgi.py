7061fc4ec3b17b8f14e58c1d146a52bc212911fc
Arun Ravindran <arunvr@gmail.com>
2018-06-11 17:21:03 +0530

    Removed whitenoise

--- src/superbook/wsgi.py
+++ src/superbook/wsgi.py
@@ -10,10 +10,10 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+#from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "src.superbook.settings.production")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
